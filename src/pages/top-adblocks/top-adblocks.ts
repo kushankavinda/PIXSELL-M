4679976809abcc61fc9b55b2c62289967816e678
kushankavinda <kaavinda92@gmail.com>
2017-11-22 14:14:06 +0530

    Update top-adblocks.ts

--- src/pages/top-adblocks/top-adblocks.ts
+++ src/pages/top-adblocks/top-adblocks.ts
@@ -2,10 +2,6 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 import { AngularFireDatabase } from 'angularfire2/database';
-import { Observable } from 'rxjs/Oimport { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-import { AngularFireDatabase } from 'angularfire2/database';
 import { Observable } from 'rxjs/Observable';
 
 
@@ -74,44 +70,4 @@ export interface Records{
   progressRate : number,
   recordCount : number,
   timeStamp : string
-}
-bservable';
-
-
-@IonicPage()
-@Component({
-  selector: 'page-top-adblocks',
-  templateUrl: 'top-adblocks.html',
-  
-})
-export class TopAdblocksPage {
-  ADblocks: Observable<Adblocks[]>; 
-
-  constructor(public navCtrl: NavController, public navParams: NavParams,afDB:AngularFireDatabase) 
-  {
-    this.ADblocks = afDB.list('Ad-blocks', ref => ref.orderByChild('progressRate')
-          .limitToLast(10)).valueChanges();
-    //this.ADblocks = afDB.list('Ad-blocks').valueChanges();  
-  }
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad TopAdblocksPage');
-  }
-
-}
-
-export interface Adblocks {
-  availablePx:number,
-  basePrice: number,
-  blockId: string|number,
-  blockName: string,
-  currentPrice: number,
-  duration: number,
-  expectPrice : number,
-  numberOfPx: number,
-  ownerSetRisk: string,
-  progressRate:number,
-  siteName: string,
-  starts: string
-    
 }
